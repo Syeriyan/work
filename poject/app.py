1bdc3275d0ee607078539b8c21302de7960ca8ce
syed riyan <syedriyan9819@gmail.com>
2026-02-12 14:08:53 +0530

    mam

--- poject/app.py
+++ poject/app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask App!"
+    return "Flask App is Running Successfully ✅"
+
+@app.route("/time")
+def time():
+    return f"Server Time: {datetime.datetime.now()}"
+
+@app.route("/health")
+def health():
+    return "OK"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
